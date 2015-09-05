1d6e5311262d35346fe9c4a63ec64a6e6c87b0f1
fkeck <francois.keck@gmail.com>
2015-09-05 20:49:41 +0200

    New functions for legends

--- R/codeinc.R
+++ R/codeinc.R
@@ -1,8 +1,4 @@
-#' Generate chunks of HTML/JS code
-#'
-#' These functions are used internally by \code{\link{writeMap}} to generate chunks of HTML and Javascript code.
-#'
-#'@rdname chunks
+
 incLeaflet <- function(loc){
   if(loc == "online"){
     res <- paste("<link rel=\"stylesheet\" href=\"http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css\" />
@@ -14,27 +10,23 @@ incLeaflet <- function(loc){
 return(res)
 }
 
-#'@rdname chunks
 incEncoding <- function(code){
   res <- paste("<meta charset=", code, ">", sep="")
   return(res)
 }
 
-#'@rdname chunks
 incPopupCSS <- function(height, width){
-  res <- paste("<style type=\"text/css\">
-\t.leaflet-popup-content {
-\t\tpadding-right:20px !important;
-\t\twidth:auto !important;
-\t\tmax-width:", width*0.75, "px !important;
-\t\tmax-height:", height*0.75, "px !important;
-\t\toverflow:auto !important;
-\t}
-  </style>", sep="")
+  res <- paste0(
+".leaflet-popup-content {
+  padding-right:20px !important;
+  width:auto !important;
+  max-width:", width*0.75, "px !important;
+  max-height:", height*0.75, "px !important;
+  overflow:auto !important;
+}")
   return(res)
 }
 
-#'@rdname chunks
 incData <- function(prefix){
   paste("<script src=\"", prefix,"_data/", prefix,"_datapoints.js\"></script>
 <script src=\"", prefix,"_data/", prefix,"_dataicons.js\"></script>
@@ -43,14 +35,47 @@ incData <- function(prefix){
   sep="")
 }
 
-#'@rdname chunks
 initMap0 <- function(height, width){
   paste("<div id=\"map\" class=\"map\" style=\"height: ", height,"px; width: ",width, "px\"></div>", sep="")
 }
 
-#'@rdname chunks
 initMap1 <- function(setView, setZoom){
   if(is.null(setView)) setView <- c(0, 0)
   if(is.null(setZoom)) setZoom <- 1
   paste("var map = L.map('map', {zoomControl:false, attributionControl:false}).setView([", setView[1], ", ", setView[2],"], ", setZoom, ");", sep="")
+}
+
+incInfoPanelCSS <- function(){
+  ".info {
+    padding: 6px 8px;
+    background: rgba(255,255,255,0.8);
+    box-shadow: 0 0 15px rgba(0,0,0,0.2);
+    border-radius: 5px;
+  }"
+}
+
+incLegendCSS <- function(){
+  ".legend i {
+    clear: left;
+    float: left;
+    margin-right: 8px;
+    margin-top: 5px;
+  }
+  .legend p {
+    float: left;;
+    line-height: 5px;
+  }
+  .legend h1 {
+    font-size: 10px;
+    margin-top: 0px;
+    margin-bottom: 2px;
+  }
+  .legend hr {
+    margin-top: 2px;
+    margin-bottom: 2px;
+    display: block;
+	  height: 1px;
+    border: 0;
+    border-top: 1px solid #AAAAAA;
+  }"
 }
