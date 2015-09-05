2418f906757015b9bacb30482cc3631f27e40a33
fkeck <francois.keck@gmail.com>
2015-09-05 20:51:30 +0200

    Add legend and popup.rmd support

--- R/splayer.R
+++ R/splayer.R
@@ -2,12 +2,10 @@
 #'
 #'Define a new data layer from an object sp.
 #'
-#'@param x a spatial object as defined in the package \pkg{sp}.
-#'@param name a character string to name the layer.
-#'@param label a reserved argument (in development).
-#'@param popup a character vector giving contents for popups. HTML tags are accepted.
+#' @param x a spatial object as defined in the package \pkg{sp}.
+#' @param ... additional arguments to pass to the function.
 #'
-#'@examples \dontrun{
+#' @examples \dontrun{
 #'  #POINTS
 #'  data(velov)
 #'  vv <- spLayer(velov, stroke=F, popup=velov$NAME)
@@ -22,12 +20,15 @@
 #'  writeMap(bm1, cs, vv)
 #'}
 #'@export
-spLayer <- function(x, name=NULL, label, popup, ...){
+spLayer <- function(x, ...){
   UseMethod("spLayer", x)
 }
 
 #'Define a Vector Data Layer
-#'@export
+#'
+#' @param x a spatial object as defined in the package \pkg{sp}.
+#' @param ... additional arguments to pass to the function.
+#' @export
 spLayer.default <- function(x, ...){
   print("Error: x not recognized as Spatial* object")
 }
@@ -45,8 +46,7 @@ spLayer.default <- function(x, ...){
 #'@param x a spatial object (see Details).
 #'@param name a character string to name the layer.
 #'@param size a numerical vector giving the size of points (radius in pixels).
-#'@param png character vector giving the paths for the PNG icons.
-#'If \code{NULL} (default), circles are drawn.
+#'@param png character vector giving the paths for the PNG icons. If \code{NULL} (default), circles are drawn.
 #'@param png.width,png.height numerical vectors giving the PNG icons dimensions on the map (in pixels).
 #'@param stroke logical. Should a stroke be drawn along lines and polygons?
 #'@param stroke.col a vector of any of the three kinds of \R color specifications to set strokes color.
@@ -58,12 +58,15 @@ spLayer.default <- function(x, ...){
 #'@param fill.alpha a vector of numeric values in \eqn{[0, 1]} setting fill opacity.
 #'@param label a reserved argument (in development).
 #'@param popup a character vector giving contents for popups. HTML tags are accepted.
+#'@param popup.rmd a logical indicating whether the popups should be processed as R Markdown with \pkg{knitr}. Default \code{FALSE}.
+#'@param legend a legend object created with \code{\link{layerLegend}}.
+#'@param ... additional arguments to pass to the function.
 #'
-#'@rdname splayer_methods
+#'@method spLayer SpatialPoints
 #'@export
 spLayer.SpatialPoints <- function(x, name = NULL, png = NULL, size = 5, png.width = 15, png.height = 15,
-                                  stroke = T, stroke.col = 1, stroke.lwd = 1, stroke.lty = 1, stroke.alpha = 1,
-                                  fill = T, fill.col = 2, fill.alpha = 0.5,
+                                  stroke = TRUE, stroke.col = 1, stroke.lwd = 1, stroke.lty = 1, stroke.alpha = 1,
+                                  fill = TRUE, fill.col = 2, fill.alpha = 0.5,
                                   label = NULL, popup = "", popup.rmd = FALSE, legend = NULL, ...){
     if(is.null(png)){
       
@@ -145,25 +148,36 @@ spLayer.SpatialPoints <- function(x, name = NULL, png = NULL, size = 5, png.widt
       tested.names <- c("png", "size", "label", "popup")[tested.index]
       names(tab) <- tested.names
       
+      legend$layer <- name
       tab$name <- name
+      tab$legend <- legend
       tab$coords <- coordinates(x)
       
       if("label" %in% tested.names)
         tab$label <- paste("\"", as.character(tab$label), "\"", sep="")
-      if("popup" %in% tested.names)
-        tab$popup <- paste("\"", as.character(tab$popup), "\"", sep="")
-      
+      if("popup" %in% tested.names){
+        if(popup.rmd){
+          tab$popup <- sapply(as.vector(tab$popup), function(x) knitr::knit2html(output = NULL, text = x,
+                                                                                 options = c('fragment_only', 'base64_images')))
+          tab$popup <- gsub("\\n", "<br>", tab$popup)
+          tab$popup <- gsub("\"", "\\\\\"", tab$popup)
+        }
+          tab$popup <- paste("\"", as.character(tab$popup), "\"", sep="")
+      }
       class(tab) <- c("splicons")
       return(tab)
     }
 }
 
-#'@rdname splayer_methods
+#'Define a Vector Data Layer
+#'
+#'@inheritParams spLayer.SpatialPoints
+#'
+#'@method spLayer SpatialLines
 #'@export
 spLayer.SpatialLines <- function(x, name = NULL,
                                  stroke = TRUE, stroke.col = 1, stroke.lwd = 1, stroke.lty = 1, stroke.alpha = 1,
-                                 fill = TRUE, fill.col = 2, fill.alpha = 0.5,
-                                 label = NULL, popup = "", legend = NULL, ...){
+                                 label = NULL, popup = "", popup.rmd = FALSE, legend = NULL, ...){
   
   if(!inherits(x, "SpatialLines"))
     stop("x must be an object of class SpatialLines or SpatialLinesDataFrame")
@@ -191,7 +205,9 @@ spLayer.SpatialLines <- function(x, name = NULL,
   tab$strokeAlpha[!stroke.logical] <- 0
   tab$strokeCol <- col2hexa(tab$strokeCol)
   
+  legend$layer <- name
   tab$name <- name
+  tab$legend <- legend
   tab$coords <- coordinates(x)
   
   if (any(as.numeric(tab$strokeAlpha)<0) || any(as.numeric(tab$strokeAlpha)>1))
@@ -201,19 +217,30 @@ spLayer.SpatialLines <- function(x, name = NULL,
   
   if("label" %in% tested.names)
     tab$label <- paste("\"", as.character(tab$label), "\"", sep="")
-  if("popup" %in% tested.names)
+  if("popup" %in% tested.names){
+    if(popup.rmd){
+      tab$popup <- sapply(as.vector(tab$popup), function(x) knitr::knit2html(output = NULL, text = x,
+                                                                             options = c('fragment_only', 'base64_images')))
+      tab$popup <- gsub("\\n", "<br>", tab$popup)
+      tab$popup <- gsub("\"", "\\\\\"", tab$popup)
+    }
     tab$popup <- paste("\"", as.character(tab$popup), "\"", sep="")
-  
+  }
   class(tab) <- c("spllines")
   return(tab)
 }
 
-#'@rdname splayer_methods
+#'Define a Vector Data Layer
+#'
+#'@inheritParams spLayer.SpatialPoints
+#'@param holes a logical indicating whether to use the \code{hole} slots of the \code{SpatialPolygons} object.
+#'
+#'@method spLayer SpatialPolygons
 #'@export
 spLayer.SpatialPolygons <- function(x, name = NULL,
                                     stroke = TRUE, stroke.col = 1, stroke.lwd = 1, stroke.lty = 1, stroke.alpha = 1,
                                     fill = TRUE, fill.col = 2, fill.alpha = 0.5,
-                                    label = NULL, popup = "", holes = FALSE, legend = NULL, ...){
+                                    label = NULL, popup = "", popup.rmd = FALSE, holes = FALSE, legend = NULL, ...){
   if(!inherits(x, "SpatialPolygons"))
     stop("x must be an object of class SpatialPolygons or SpatialPolygonsDataFrame")
   
@@ -247,7 +274,9 @@ spLayer.SpatialPolygons <- function(x, name = NULL,
   tab$fillAlpha[!fill.logical] <- 0
   tab$fillCol <- col2hexa(tab$fillCol)
   
+  legend$layer <- name
   tab$name <- name
+  tab$legend <- legend
   tab$coords <- polycoords(x)
   if (holes){
     tab$holes <- polyholes(x)
@@ -265,9 +294,15 @@ spLayer.SpatialPolygons <- function(x, name = NULL,
   
   if("label" %in% tested.names)
     tab$label <- paste("\"", as.character(tab$label), "\"", sep="")
-  if("popup" %in% tested.names)
+  if("popup" %in% tested.names){
+    if(popup.rmd){
+      tab$popup <- sapply(as.vector(tab$popup), function(x) knitr::knit2html(output = NULL, text = x,
+                                                                             options = c('fragment_only', 'base64_images')))
+      tab$popup <- gsub("\\n", "<br>", tab$popup)
+      tab$popup <- gsub("\"", "\\\\\"", tab$popup)
+    }
     tab$popup <- paste("\"", as.character(tab$popup), "\"", sep="")
-  
+  }
   class(tab) <- c("splpolygons")
   return(tab)
   
@@ -282,9 +317,14 @@ spLayer.SpatialPolygons <- function(x, name = NULL,
 #'@param layer which layer to select?
 #'@param cells.col a vector of any of the three kinds of \R color specifications giving a gradient to color the grid.
 #'@param cells.alpha a vector of numeric values in \eqn{[0, 1]} setting grid opacity.
+#'@param legend a legend object created with \code{\link{layerLegend}}.
+#'@param ... additional arguments to pass to the function.
+#'
+#'@method spLayer SpatialGridDataFrame
 #'@export
 spLayer.SpatialGridDataFrame <- function(x, name = NULL, layer,
-                                         cells.col = heat.colors(12), cells.alpha = 1, legend = NULL, ...){
+                                         cells.col = heat.colors(12), cells.alpha = 1,
+                                         legend = NULL, ...){
   if(!inherits(x, "SpatialGridDataFrame"))
     stop("x must be an object of class SpatialGridDataFrame")
   spLayerControl(name = name, legend = legend)
@@ -302,8 +342,8 @@ spLayer.SpatialGridDataFrame <- function(x, name = NULL, layer,
   x <- as(x, "SpatialGridDataFrame")
   
   cells.col <- col2hexa(cells.col, alpha.channel = TRUE, alpha = cells.alpha, charstring = FALSE)
-  
-  tab <- list(x = x, name = name, cells.col = cells.col, x.bbox = x.bbox)
+  legend$layer <- name
+  tab <- list(x = x, name = name, cells.col = cells.col, x.bbox = x.bbox, legend = legend)
   
   class(tab) <- c("splgrid")
   return(tab)
@@ -313,6 +353,8 @@ spLayer.SpatialGridDataFrame <- function(x, name = NULL, layer,
 #' Testing user inputs
 #'
 #' This function tests arguments validity for the function \code{\link{spLayer}}.
+#' @inheritParams spLayer.SpatialPoints
+#' @param holes a logical indicating whether to use the \code{hole} slots of the \code{SpatialPolygons} object.
 #' 
 spLayerControl <- function(name, size = 1, legend = legend,
                             stroke = TRUE, stroke.col = 1, stroke.lwd = 1, stroke.lty = 1, stroke.alpha = 1,
