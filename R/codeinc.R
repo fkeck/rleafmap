045542b34f1747a05fc853107a33544cff6dcdc2
fkeck <francois.keck@gmail.com>
2015-09-10 19:41:49 +0200

    Update html header

--- R/codeinc.R
+++ R/codeinc.R
@@ -11,7 +11,11 @@ return(res)
 }
 
 incEncoding <- function(code){
-  res <- paste("<meta charset=", code, ">", sep="")
+  res <- paste("<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" 
+\"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\">
+<meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=utf-8\" />", sep="")
   return(res)
 }
 
